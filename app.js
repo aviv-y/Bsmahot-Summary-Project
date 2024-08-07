cb23a4e897e58588280131835b4ea53d6365d34c
AvivMali <aviv0583207464@gmail.com>
2024-08-07 23:23:24 +0300

    add cors function

--- app.js
+++ app.js
@@ -372,6 +372,17 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+//מניעת שגיאת קורס
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
 const connectionParams = {
   useNewUrlParser: true,
   useCreateIndex: true,
